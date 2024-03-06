998fa3fd575d8eb1e5b573a7c11b4292b8865004
Higor <higorjugler@hotmail.com>
2024-03-06 18:17:43 -0300

    first commit

---
+++ SearchFrete/Login.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  SearchFrete
+//
+//  Created by Higor Jugler on 3/6/24.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ SearchFrete/Routes.swift
@@ -0,0 +1,18 @@
+//
+//  Routes.swift
+//  SearchFrete
+//
+//  Created by Higor Jugler on 3/6/24.
+//
+
+import SwiftUI
+
+struct Routes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Routes()
+}
